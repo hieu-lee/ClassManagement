e700d9e609a8cd92f2d2e8f88567c6d5adf1a80c
Hieu Le <hieuld1009@gmail.com>
2021-05-08 09:36:16 +0700

    .

--- ClassManagement/Startup.cs
+++ ClassManagement/Startup.cs
@@ -42,6 +42,7 @@ namespace ClassManagement
                 config.SnackbarConfiguration.ShowTransitionDuration = 500;
                 config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
             });
+            // services.AddScoped<GradesService>();
             services.AddScoped<ClassesService>();
             services.AddSingleton<WeatherForecastService>();
         }
