42304c42d8c5d7a0decc194eedd130bc863ad2ea
hieu-lee <hiuld1009@gmail.com>
2021-05-25 20:56:40 +0700

    Dapper doesn't support TimeSpan so I have to do it myself

---
+++ ClassManagement/Data/TimeSpanHandler.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassManagement.Data
+{
+    public abstract class SqliteTypeHandler<T> : SqlMapper.TypeHandler<T>
+    {
+        // Parameters are converted by Microsoft.Data.Sqlite
+        public override void SetValue(IDbDataParameter parameter, T value)
+            => parameter.Value = value;
+    }
+    public class TimeSpanHandler : SqliteTypeHandler<TimeSpan>
+    {
+        public override TimeSpan Parse(object value)
+        => TimeSpan.Parse((string)value);
+    }
+}
