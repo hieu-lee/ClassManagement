7473ef04399f18bbf849b428188c420ffb7569da
hieu-lee <hiuld1009@gmail.com>
2021-05-27 20:42:20 +0700

    Complete SessionService

--- ClassManagement/Services/SessionService.cs
+++ ClassManagement/Services/SessionService.cs
@@ -3,12 +3,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace ClassManagement.Services
 {
+
     public class SessionService
     {
-        public HashSet<ClassSchedule> NotifiedSchedules { get; set; } = new();
-        public bool NotificationTimerStarted { get; set; } = false;
+        private readonly ProtectedLocalStorage localStorage;
+        public bool LoggedState => !string.IsNullOrWhiteSpace(UsernameState);
+#nullable enable
+        public string? UsernameState { get; set; }
+#nullable disable
+
+        public SessionService(ProtectedLocalStorage localStorage)
+        {
+            this.localStorage = localStorage;
+        }
+
+        public async Task SignInAsync(Account account)
+        {
+            var task = localStorage.SetAsync("MEDUUsernameState", account.Username);
+            UsernameState = account.Username;
+            await task;
+        }
+
+        public async Task SignUpAsync(Account account)
+        {
+            var task = localStorage.SetAsync("MEDUUsernameState", account.Username);
+            UsernameState = account.Username;
+            await task;
+        }
+
+        public async Task SignOutAsync()
+        {
+            var task = localStorage.DeleteAsync("MEDUUsernameState");
+            UsernameState = null;
+            await task;
+        }
     }
 }
